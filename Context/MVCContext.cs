b19c0bd5ffd93ee9c16a5fe7c7fb10388b5f1641
maddy-222 <91879300+maddy-222@users.noreply.github.com>
2022-11-02 02:27:41 +0530

    Add files via upload

---
+++ Context/MVCContext.cs
@@ -0,0 +1,17 @@
+﻿using EFMVC_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFMVC_CRUD.Context
+{
+    public class MVCContext: DbContext
+    {
+        public MVCContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
