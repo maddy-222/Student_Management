a7bf9f2b85129286813349555f471ee740b3e264
maddy-222 <91879300+maddy-222@users.noreply.github.com>
2022-11-02 01:47:35 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFMVC_CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string studentAddress { get; set; }
+        public int studentAge { get; set; }
+
+        public Subject Subject { get; set; } 
+    }
+}
---
+++ Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFMVC_CRUD.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int subjectId { get; set; }
+        public string subjectName { get; set; }
+        public string subjectTerm { get; set; }
+        public int subjectCredets { get; set; }
+        public List<Student> SubjectStudents { get; set; } = new List<Student>();
+    }
+}
